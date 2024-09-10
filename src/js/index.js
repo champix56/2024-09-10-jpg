2dc2756bb7ff8f5fbc92be5707e0b3d4b02fa88b
champix56 <desorbaix@free.fr>
2024-09-10 17:29:24 +0100

    fin j1

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * initialisation pour events d'index
+ * @param {string} a context du doc
+ * @param {number} b hnbdfg
+ * @param {*} c nhdryjdtyjddt
+ */
+function initialisation(a,b,c) {
+    var a = document.querySelector('#header .js');
+    a.addEventListener('click',function (evt) {
+    })
+}
+
+initialisation(null,rf,fuk)
+
+//document.addEventListener('DOMContentLoaded',initialisation);
